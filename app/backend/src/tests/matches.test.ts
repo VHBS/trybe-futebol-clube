6a82f1d5d460d7290757ab895afeb49d59d7bad5
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-06-03 02:07:30 -0300

    test: Match retorna as partidas em progresso e as terminadas

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -7,7 +7,7 @@ import { app } from '../app';
 
 import { Response } from 'superagent';
 import Match from '../database/models/Match';
-import { allMatches } from './mocks/matchMocks';
+import { allMatches, allMatchesEnded, allMatchesInProgress } from './mocks/matchMocks';
 
 chai.use(chaiHttp);
 
@@ -37,6 +37,52 @@ describe('Matches', () => {
   
       expect(chaiHttpResponse.status).to.be.equal(200);
       expect(matches).deep.equal(allMatches)
+    });
+  });
+
+  describe('Retorna todos as partidas em andamento', () => {
+    beforeEach(() => {
+      sinon
+        .stub(Match, "findAll")
+        .resolves(allMatchesInProgress);
+    });
+  
+    afterEach(()=>{
+      (Match.findAll as sinon.SinonStub).restore();
+    })
+  
+    it('Retorna status 200 com as partidas', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/matches?inProgress=true')
+      
+        const matches = chaiHttpResponse.body;
+  
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(matches).deep.equal(allMatchesInProgress)
+    });
+  });
+
+  describe('Retorna todos as partidas finalizadas', () => {
+    beforeEach(() => {
+      sinon
+        .stub(Match, "findAll")
+        .resolves(allMatchesEnded);
+    });
+  
+    afterEach(()=>{
+      (Match.findAll as sinon.SinonStub).restore();
+    })
+  
+    it('Retorna status 200 com as partidas', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/matches?inProgress=false')
+      
+        const matches = chaiHttpResponse.body;
+  
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(matches).deep.equal(allMatchesEnded)
     });
   });
 });
--- app/backend/src/tests/mocks/matchMocks.ts
+++ app/backend/src/tests/mocks/matchMocks.ts
@@ -1,6 +1,5 @@
 import Match from '../../database/models/Match';
 
-
 const allMatches = [
   {
     id: 1,
@@ -32,4 +31,66 @@ const allMatches = [
   }
 ] as Match[];
 
-export { allMatches }
+const allMatchesInProgress = [
+	{
+		id: 41,
+		homeTeam: 16,
+		homeTeamGoals: 2,
+		awayTeam: 9,
+		awayTeamGoals: 0,
+		inProgress: true,
+		teamHome: {
+			teamName: "São Paulo"
+		},
+		teamAway: {
+			teamName: "Internacional"
+		}
+	},
+	{
+		id: 42,
+		homeTeam: 6,
+		homeTeamGoals: 1,
+		awayTeam: 1,
+		awayTeamGoals: 0,
+		inProgress: true,
+		teamHome: {
+			teamName: "Ferroviária"
+		},
+		teamAway: {
+			teamName: "Avaí/Kindermann"
+		}
+	}
+] as Match[];
+
+const allMatchesEnded = [
+	{
+		id: 1,
+		homeTeam: 16,
+		homeTeamGoals: 1,
+		awayTeam: 8,
+		awayTeamGoals: 1,
+		inProgress: false,
+		teamHome: {
+			teamName: "São Paulo"
+		},
+		teamAway: {
+			teamName: "Grêmio"
+		}
+	},
+	{
+		id: 2,
+		homeTeam: 9,
+		homeTeamGoals: 1,
+		awayTeam: 14,
+		awayTeamGoals: 1,
+		inProgress: false,
+		teamHome: {
+			teamName: "Internacional"
+		},
+		teamAway: {
+			teamName: "Santos"
+		}
+	}
+] as Match[];
+
+export { allMatches, allMatchesInProgress, allMatchesEnded }
